e5e69a683c485fc4d0553c16d43c3eb0593eb13e
hulang1024 <1013644379@qq.com>
2018-10-27 16:43:31 +0800

    .

--- ide.js
+++ ide.js
@@ -1,14 +1,15 @@
 window.onload = function() {
+  window.game = new Game();
+  exportMethods(game);
+  var editor = new Editor();
+  new Toolbar(game, editor);
+
   var isMobile = function() {
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
   }
   if (isMobile() && !(window.innerWidth > window.innerHeight)) {
     alert('检测到您当前使用竖屏，为了更好的体验，请切换到横屏：）');
   }
-  window.game = new Game();
-  exportMethods(game);
-  var editor = new Editor();
-  new Toolbar(game, editor);
 }
 
 function exportMethods(game) {
